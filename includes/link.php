622b507754e6221c17040f3116f58a568caef1b6
RuanParreira <ruan.parreira@edu.uniube.br>
2025-04-10 21:40:29 -0300

    Banco

--- includes/link.php
+++ includes/link.php
@@ -1,8 +1,13 @@
 <?php
-$servidor = "localhost";
-$usuario = "root";
-$senha = "";
-$dbname = "organizalab";
+// $servidor = "localhost";
+// $usuario = "root";
+// $senha = "";
+// $dbname = "organizalab";
+
+$servidor = "sql101.infinityfree.com";
+$usuario = "if0_38721638";
+$senha = "nyhpzWIPbdeQ5DG";
+$dbname = "if0_38721638_organizalab";
 
 $conn = new mysqli($servidor, $usuario, $senha, $dbname);
 if ($conn->connect_error) {
