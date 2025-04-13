ea27f698bbf7c48585462af4894f77557a5c0a09
RuanParreira <ruan.parreira@edu.uniube.br>
2025-04-13 20:12:58 -0300

    LinkBanco

--- includes/link.php
+++ includes/link.php
@@ -1,8 +1,13 @@
 <?php
+// $servidor = "localhost";
+// $usuario = "root";
+// $senha = "";
+// $dbname = "organizalab";
+
 $servidor = "localhost";
-$usuario = "root";
-$senha = "";
-$dbname = "organizalab";
+$usuario = "u637029734_Admin";
+$senha = "6[q+3r;!C";
+$dbname = "u637029734_organizalab";
 
 $conn = new mysqli($servidor, $usuario, $senha, $dbname);
 if ($conn->connect_error) {
