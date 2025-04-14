808e59d01935d356e5ba94693f5f7581e780407a
RuanParreira <ruan.parreira@edu.uniube.br>
2025-04-13 23:16:08 -0300

    Autalizando

--- includes/link.php
+++ includes/link.php
@@ -6,11 +6,6 @@ $env = parse_ini_file(__DIR__ . '/../.env');
 // $senha = "";
 // $dbname = "organizalab";
 
-$servidor = "localhost";
-$usuario = "u637029734_Admin";
-$senha = "6[q+3r;!C";
-$dbname = "u637029734_organizalab";
-
 $conn = new mysqli($servidor, $usuario, $senha, $dbname);
 if ($conn->connect_error) {
     die("Falha na conexão" . $conn->connect_error);
