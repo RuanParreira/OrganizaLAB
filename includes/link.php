d3c697081ea7d0d500cdf282dcef8486d13cfc2a
RuanParreira <ruan.parreira@edu.uniube.br>
2025-04-13 23:19:50 -0300

    Segurança

--- includes/link.php
+++ includes/link.php
@@ -6,6 +6,12 @@ $env = parse_ini_file(__DIR__ . '/../.env');
 // $senha = "";
 // $dbname = "organizalab";
 
+$servidor = $env['DB_SERVER'];
+$usuario = $env['DB_USER'];
+$senha = $env['DB_PASSWORD'];
+$dbname = $env['DB_NAME'];
+
+
 $conn = new mysqli($servidor, $usuario, $senha, $dbname);
 if ($conn->connect_error) {
     die("Falha na conexão" . $conn->connect_error);
